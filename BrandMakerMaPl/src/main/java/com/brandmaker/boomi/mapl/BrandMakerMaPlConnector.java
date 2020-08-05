c114d1b7c697f77463e512757719b2a3a3b5c0ed
Axel Amthor <axel.amthor@brandmaker.com>
2020-08-05 10:30:40 +0200

    parent added

--- BrandMakerMaPl/src/main/java/com/brandmaker/boomi/mapl/BrandMakerMaPlConnector.java
+++ BrandMakerMaPl/src/main/java/com/brandmaker/boomi/mapl/BrandMakerMaPlConnector.java
@@ -1,5 +1,7 @@
 package com.brandmaker.boomi.mapl;
 
+import java.util.logging.Logger;
+
 import com.boomi.connector.api.BrowseContext;
 import com.boomi.connector.api.Browser;
 import com.boomi.connector.api.Operation;
@@ -8,18 +10,23 @@ import com.boomi.connector.util.BaseConnector;
 
 public class BrandMakerMaPlConnector extends BaseConnector {
 
+	private static final Logger Logger = java.util.logging.Logger.getLogger(BrandMakerMaPlConnector.class.getName());
+	
     @Override
     public Browser createBrowser(BrowseContext context) {
+    	Logger.severe( ((OperationContext)context).getObjectTypeId());
         return new BrandMakerMaPlBrowser(createConnection(context));
     }    
 
     @Override
     protected Operation createGetOperation(OperationContext context) {
+    	Logger.severe( context.getObjectTypeId());
         return new BrandMakerMaPlGetOperation(createConnection(context));
     }
 
     @Override
     protected Operation createQueryOperation(OperationContext context) {
+    	Logger.severe( context.getObjectTypeId());
         return new BrandMakerMaPlQueryOperation(createConnection(context));
     }
    
